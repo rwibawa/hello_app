ce923002b98735f72fc2caac42045cafd31a3881
Ryan Wibawa <ryan.wibawa@gmail.com>
2015-04-07 05:56:11 +0000

    omniauth-facebook

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'application#hello'
+  #root 'application#hello'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+get 'auth/:provider/callback', to: 'sessions#create'
+get 'logout', to: 'sessions#destroy'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
